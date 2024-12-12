012f20afc6115ca82ebb3d1da1f3d73c18c5595f
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2024-12-12 10:44:00 +0100

    If sats som skriver ut welcome ifall namnet är "noname" och skriver "Your name is not noname" ifall användaren skriver ett annat namn

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //if sats som skriver ut "hello world" om 6 är större eller lika med 3
         if (6>=3){
             System.out.println("Hello world");
+        }
+        //hämtar namn från användaren
+        Scanner tb = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String name = tb.nextLine();
+        //kontrollerar att namnet är korrekt och skriver ut "Welcome" annars skriver den att den inte är noname
+        if ("noname".equals(name)){
+            System.out.println("Welcome!");
+        }
+        else{
+            System.out.println("Your name is not noname");
         }
     }
 }
