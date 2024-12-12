9002a636bf2485217c7863853b97b8f35232eb9e
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2024-12-12 10:31:41 +0100

    Skapat en if sats sm skriver ut "Hello world" om 6 är större eller lika med 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //if sats som skriver ut "hello world" om 6 är större eller lika med 3
+        if (6>=3){
+            System.out.println("Hello world");
+        }
     }
 }
